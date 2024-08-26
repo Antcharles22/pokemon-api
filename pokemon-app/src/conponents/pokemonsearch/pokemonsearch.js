17e5030f61732fe8eafd1e3e95caf21260aeb513
Antcharles22 <121199104+Antcharles22@users.noreply.github.com>
2024-08-26 14:39:18 +0100

    added grid not fin

--- pokemon-app/src/conponents/pokemonsearch/pokemonsearch.js
+++ pokemon-app/src/conponents/pokemonsearch/pokemonsearch.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import fetchData from "../pokiapi/pokiapi.js";
+import "./pokemonSearch.css";
+
+
 
 function PokemonSearch() {
   const [pokemonData, setPokemonData] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
+  const [pokemonGrid, setPokemonGrid] = useState([]);
 
   const handleSearch = async () => {
     try {
@@ -14,8 +18,15 @@ function PokemonSearch() {
     }
   };
 
+  const addToGrid = () => {
+    if (pokemonData && pokemonGrid.length < 6) {
+      setPokemonGrid([...pokemonGrid, pokemonData]);
+      setPokemonData(null); // Clear the current search result
+      setSearchTerm(""); // Clear the search input
+    }
+};
   return (
-    <div>
+    <div className="info">
       <input 
         type="text" 
         id="pokemonSearch" 
@@ -23,19 +34,38 @@ function PokemonSearch() {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <button onClick={handleSearch}>Search</button>
+      <button onClick={handleSearch} className="btm">Search</button>
       {pokemonData && (
-        <div>
-          <h3>{pokemonData.name}</h3>
-          <p>Type: {pokemonData.types.map(typeInfo => typeInfo.type.name).join(", ")}</p>
-          <img 
-            src={pokemonData.sprites.front_default} 
-            alt={pokemonData.name} 
-            id="pokemonImage" 
-            style={{ display: "block" }} 
-          />
+        <div className="pokiBox">
+            <article className="pokiInfo">
+               <h3>{pokemonData.name}</h3>
+          
+               <img 
+                src={pokemonData.sprites.front_default} 
+               alt={pokemonData.name} 
+               id="pokemonImage"
+              style={{ display: "block" }} />
+              <p> Type: {pokemonData.types.map(typeInfo => typeInfo.type.name).join(", ")}</p>
+          </article>
+            <button onClick={addToGrid} className="btm">Add to Grid</button>
         </div>
       )}
+      
+      <div className="grid-container">
+      <div className="pokemonGrid">
+        {pokemonGrid.map((pokemon, index) => (
+          <div key={index} className="pokemonCard">
+            <h3>{pokemon.name}</h3>
+            <img 
+              src={pokemon.sprites.front_default} 
+              alt={pokemon.name}
+              className="pokemonImage" 
+            />
+            <p> Type: {pokemon.types.map(typeInfo => typeInfo.type.name).join(", ")}</p>
+          </div> 
+        ))}
+      </div>
+        </div>
     </div>
   );
 }
