f93d582f8202fdfb01174d92cdba2b803fc907d9
Antcharles22 <121199104+Antcharles22@users.noreply.github.com>
2024-09-27 11:12:22 +0100

    small details

--- pokemon-app/src/conponents/pokemonsearch/pokemonsearch.js
+++ pokemon-app/src/conponents/pokemonsearch/pokemonsearch.js
@@ -6,7 +6,7 @@ import remove from "../path/to/svg.js"; // Import the remove SVG
 // Utility function to capitalize the first letter of a string
 const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
-}; 
+};
 
 function PokemonSearch() {
   const [pokemonData, setPokemonData] = useState("");
@@ -15,7 +15,7 @@ function PokemonSearch() {
   const [errorMessage, setErrorMessage] = useState(""); // Add state for error message
   const [maxPokemonMessage, setMaxPokemonMessage] = useState(""); // Add state for max Pokémon message
   const prevGridLength = useRef(pokemonGrid.length); // Use ref to keep track of previous grid length
-  
+
 
   const handleSearch = async () => {
     if (!searchTerm.trim()) {
@@ -41,90 +41,90 @@ function PokemonSearch() {
     } else if (pokemonGrid.length >= 6) {
       setMaxPokemonMessage("Maximum 6 Pokémon"); // Set max Pokémon message
     }
-    };
+  };
 
-const handleInputChange = (e) => {
-  setSearchTerm(e.target.value);
-  setErrorMessage(""); // Clear error message when input changes
-};
-const removeFromGrid = (index) => {
-  const newGrid = [...pokemonGrid];
-  newGrid.splice(index, 1);
-  setPokemonGrid(newGrid);
-};
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+    setErrorMessage(""); // Clear error message when input changes
+  };
+  const removeFromGrid = (index) => {
+    const newGrid = [...pokemonGrid];
+    newGrid.splice(index, 1);
+    setPokemonGrid(newGrid);
+  };
 
-useEffect(() => {
-  // Play the audio for the newly added Pokémon
-  if (pokemonGrid.length > prevGridLength.current) {
-    const lastAddedPokemon = pokemonGrid[pokemonGrid.length - 1];
-    const audio = new Audio(lastAddedPokemon.cries.latest);
-    audio.play();
-  }
-  prevGridLength.current = pokemonGrid.length; // Update the previous grid length
-}, [pokemonGrid]);
+  useEffect(() => {
+    // Play the audio for the newly added Pokémon
+    if (pokemonGrid.length > prevGridLength.current) {
+      const lastAddedPokemon = pokemonGrid[pokemonGrid.length - 1];
+      const audio = new Audio(lastAddedPokemon.cries.latest);
+      audio.play();
+    }
+    prevGridLength.current = pokemonGrid.length; // Update the previous grid length
+  }, [pokemonGrid]);
 
 
   return (
-   <div className="container">
+    <div className="container">
       <header>
-      <h1> Pokemon App </h1>
+        <h1> Pokemon App </h1>
       </header>
       <main className="info">
-      <input 
-        type="text" 
-        id="pokemonSearch" 
-        placeholder="Search your Pokémon!!" 
-        value={searchTerm}
-        onChange={handleInputChange} // Update onChange handler
-      />
-      <button onClick={handleSearch} className="btm">Search</button>
-      {errorMessage && <p className="error">{errorMessage}</p>} {/* Conditionally render error message */}
-      {maxPokemonMessage && <p className="max-pokemon">{maxPokemonMessage}</p>} {/* Conditionally render max Pokémon message */}
-      {pokemonData ? (
-        <div className="pokiBox">
+        <input
+          type="text"
+          id="pokemonSearch"
+          placeholder="Search your Pokémon!!"
+          value={searchTerm}
+          onChange={handleInputChange} // Update onChange handler
+        />
+        <button onClick={handleSearch} className="btm">Search</button>
+        {errorMessage && <p className="error">{errorMessage}</p>} {/* Conditionally render error message */}
+        {maxPokemonMessage && <p className="max-pokemon">{maxPokemonMessage}</p>} {/* Conditionally render max Pokémon message */}
+        {pokemonData ? (
+          <div className="pokiBox">
             <article className="pokiInfo">
-            <h3>{capitalizeFirstLetter(pokemonData.name)}</h3>
-               <img src={pokemonData.sprites.front_default} 
-               alt={pokemonData.name} 
-               id="pokemonImage"
-              style={{ display: "block" }} />
+              <h3>{capitalizeFirstLetter(pokemonData.name)}</h3>
+              <img src={pokemonData.sprites.front_default}
+                alt={pokemonData.name}
+                id="pokemonImage"
+                style={{ display: "block" }} />
               <p> Type: {pokemonData.types.map(typeInfo => typeInfo.type.name).join(", ")}</p>
-          </article>
-          <button className="addBtm"  onClick={() => addToGrid(pokemonData)}>Add to Grid</button>
-        </div>
-      ) : (
-        <div className="pokiBox placeholder">
-          <p>No Pokémon selected. Search for a Pokémon to see its details here.</p>
-        </div>
-      )}
-      
-      <div className="grid-container">
-        {pokemonGrid.length === 0 ? ( 
+            </article>
+            <button className="addBtm" onClick={() => addToGrid(pokemonData)}>Add to Grid</button>
+          </div>
+        ) : (
+          <div className="pokiBox placeholder">
+            <p>No Pokémon selected. Search for a Pokémon to see its details here.</p>
+          </div>
+        )}
+
+        <div className="grid-container">
+          {pokemonGrid.length === 0 ? (
             <p className="empty-grid-message">Create your perfect team of 6 here!</p>
           ) : (
-      <div className="pokemonGrid">
-        {pokemonGrid.map((pokemon, index) => (
-          <div key={index} className="pokemonCard">
-            <h3>{pokemon.name}</h3>
-            <img 
-              src={pokemon.sprites.front_default} 
-              alt={pokemon.name}
-              className="pokemonImage" 
-            />
-            <p> Type: {pokemon.types.map(typeInfo => typeInfo.type.name).join(", ")}</p>
-            <button onClick={() => removeFromGrid(index)} className="remove-btn">{remove()}</button> {/* Add remove button */}
-            <audio src={pokemon.cries} className="pokemonCry" /> {/* Add audio element */}
-          </div> 
-        ))}
-      </div>
+            <div className="pokemonGrid">
+              {pokemonGrid.map((pokemon, index) => (
+                <div key={index} className="pokemonCard">
+                  <h3>{pokemon.name}</h3>
+                  <img
+                    src={pokemon.sprites.front_default}
+                    alt={pokemon.name}
+                    className="pokemonImage"
+                  />
+                  <p> Type: {pokemon.types.map(typeInfo => typeInfo.type.name).join(", ")}</p>
+                  <button onClick={() => removeFromGrid(index)} className="remove-btn">{remove()}</button> {/* Add remove button */}
+                  <audio src={pokemon.cries} className="pokemonCry" />
+                </div>
+              ))}
+            </div>
           )}
         </div>
-        </main>
-        <footer className="footer">
-          <h3>Powered by Pokemon Api</h3>
-        </footer>
+      </main>
+      <footer className="footer">
+        <h3>Powered by Pokemon Api</h3>
+      </footer>
     </div>
-   
+
   );
 }
 
